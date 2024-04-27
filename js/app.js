ad451e97b1be88cbdba5fd6d1814dbda20e3ae47
mbairassem <mbairassem@gmail.com>
2024-04-27 05:18:05 +0100

    Modification fichier js, creation fichier data.js pour donnée, stockage elements dans localstorage

--- js/app.js
+++ js/app.js
@@ -1,15 +1,60 @@
+import products from "./data.js";
 let carts = document.querySelectorAll(".achat");
 
 for (let i = 0; i < carts.length; i++) {
   carts[i].addEventListener("click", (event) => {
     event.preventDefault();
-    cartNumbers();
+    cartNumbers(products[i]);
   });
 }
 
-function cartNumbers() {
+function onLoadCartNumbers() {
+  let productNumbers = localStorage.getItem("cartNumbers");
+  if (productNumbers) {
+    document.querySelector(".panier span").textContent = productNumbers;
+  }
+}
+
+/*
+    1- Recuperation de l'id de l'element pour ajout dans le localStorages
+    2- Envoir dans le panier plus incrementation
+*/
+function cartNumbers(product) {
   let productNumbers = localStorage.getItem("cartNumbers");
   productNumbers = parseInt(productNumbers);
 
-  localStorage.setItem("cartNumbers", 1);
+  if (productNumbers) {
+    localStorage.setItem("cartNumbers", productNumbers + 1);
+    document.querySelector(".panier span").textContent = productNumbers + 1;
+  } else {
+    localStorage.setItem("cartNumbers", 1);
+    document.querySelector(".panier span").textContent = 1;
+  }
+
+  setItems(product);
 }
+
+function setItems(product) {
+  let cartItems = localStorage.getItem("produitEnPanier");
+  cartItems = JSON.parse(cartItems);
+
+  if (cartItems != null) {
+    if (cartItems[product.tag] == undefined) {
+      cartItems = {
+        ...cartItems,
+        [product.tag]: product,
+      };
+    }
+
+    cartItems[product.tag].enPanier += 1;
+  } else {
+    product.enPanier = 1;
+    cartItems = {
+      [product.tag]: product,
+    };
+  }
+
+  localStorage.setItem("produitEnPanier", JSON.stringify(cartItems));
+}
+
+onLoadCartNumbers();
---
+++ js/data.js
@@ -0,0 +1,22 @@
+let products = [
+  {
+    name: "Chemise verte",
+    tag: "chemiseverte",
+    prix: 1500,
+    enPanier: 0,
+  },
+  {
+    name: "Robe rouge",
+    tag: "roderouge",
+    prix: 2000,
+    enPanier: 0,
+  },
+  {
+    name: "Echarpe gris",
+    tag: "echarpegris",
+    prix: 10000,
+    enPanier: 0,
+  },
+];
+
+export default products;
