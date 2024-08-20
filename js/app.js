ed97d7ae28bb9dc7fc4ccc70163ba3434745ff29
Isidore <aktibcode@gmail.com>
2024-08-20 04:55:24 +0100

    Ajout de la partie cart + et -

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@ for (let i = 0; i < carts.length; i++) {
   carts[i].addEventListener("click", (event) => {
     event.preventDefault();
     cartNumbers(products[i]);
+    totalCoast(products[i]);
+    // plusProduct(products[i]);
   });
 }
 
@@ -57,4 +59,116 @@ function setItems(product) {
   localStorage.setItem("produitEnPanier", JSON.stringify(cartItems));
 }
 
+
+
+function totalCoast(product){
+    // console.log('The product price is', product.price);
+    let cartCost = localStorage.getItem('totalCoast')
+
+    if (cartCost != null) {
+        cartCost = parseInt(cartCost)
+        localStorage.setItem("totalCoast", cartCost+ product.prix)
+    } else {
+        localStorage.setItem("totalCoast", product.prix)
+    }
+
+    
+}
+
+
+
+
+
+function displayCart() {
+    let cartItems = localStorage.getItem("produitEnPanier");
+  cartItems = JSON.parse(cartItems)
+  console.log(cartItems);
+  
+    let productContainer = document.querySelector(".products")
+    let cartCost = localStorage.getItem('totalCoast')
+
+    console.log(productContainer);
+    if (cartItems && productContainer) { 
+        
+        productContainer.innerHTML = '';
+
+        Object.values(cartItems).map(item => {
+            productContainer.innerHTML += `
+                
+            <div class="product" >
+                <ion-icon name="close-circle"></ion-icon>
+                <img src = "./img/produits/femmes/${item.tag}.jpg" style="width: 50px;" >
+                <span>${item.name}</span> 
+            </div> 
+            <div class="price">${item.prix} FCFA</div>
+            <div class="quantity">
+                <button class="plus" value="${item.enPanier}" id="${item.id}"> + </button>
+                <span name="${item.name}" class="valeurEnpanier">${item.enPanier}</span>
+                <button class="moins"> - </button>
+                
+            </div>  
+            <div class="total">
+                ${item.enPanier * item.prix}
+            </div>       
+            `
+            
+        })
+
+        productContainer.innerHTML += ` 
+            <div class="basketTotalContainer">
+                <h4 class="basketTotalTitle">
+                    Basket Total
+                </h4>
+                <h4 class="basketTotal">
+                    ${cartCost},00 
+                </h4>
+            </div>
+        `
+  }
+  
+}
+
+function plusProduct() {
+  const ajout = document.getElementsByClassName('plus')
+  let cartItems = localStorage.getItem("produitEnPanier");
+  let valeur = document.getElementsByClassName('valeurEnpanier')
+
+    
+  cartItems = JSON.parse(cartItems)
+  console.log(valeur)
+  
+  
+  for (var i = 0; i <= ajout.length; i++) {
+    let ajouts = ajout[i]
+    ajouts.addEventListener('click', () => {
+      console.log(Object.values(cartItems).filter((elt) => {
+        return elt.id == ajouts.id;
+      }))
+    })
+  }
+}
+
+// let  champSaisie = document.que
+
+  function avoirPanier(idP) {
+    let cartItems = localStorage.getItem("produitEnPanier");
+    cartItems = JSON.parse(cartItems)
+    Object.values(cartItems).filter((cartItem) => {
+      if(cartItem.id == idP) {
+        return console.log(cartItem.enPanier);
+      }
+    })
+
+  }
+
+
 onLoadCartNumbers();
+// 
+displayCart();
+plusProduct()
+
+
+
+
+
+
--- js/data.js
+++ js/data.js
@@ -1,20 +1,30 @@
 let products = [
   {
+    id:1,
     name: "Chemise verte",
-    tag: "chemiseverte",
+    tag: "women01",
     prix: 1500,
     enPanier: 0,
   },
   {
+    id:2,
     name: "Robe rouge",
-    tag: "roderouge",
+    tag: "women02",
     prix: 2000,
     enPanier: 0,
   },
   {
+    id:3,
     name: "Echarpe gris",
-    tag: "echarpegris",
+    tag: "women03",
     prix: 10000,
+    enPanier: 0,
+  },
+  {
+    id:4,
+    name: "Sac gris",
+    tag: "women04",
+    prix: 1000,
     enPanier: 0,
   },
 ];
